9c4dff397992ebff9eda82492c72ed2434bdaf6d
Marko <mzubanovic@gmail.com>
2023-11-22 22:28:13 +0100

    new Migration + small frontend changes

---
+++ Migrations/20231122212647_ExerciseModification.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MotionFlow.Migrations
+{
+    /// <inheritdoc />
+    public partial class ExerciseModification : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "VideoUrl",
+                table: "Exercises",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "VideoUrl",
+                table: "Exercises");
+        }
+    }
+}
--- Models/Exercise.cs
+++ Models/Exercise.cs
@@ -5,6 +5,7 @@
         public int Id { get; set; }
         public string Name { get; set; }
         public string Description { get; set; }
+        public string VideoUrl { get; set; }
         public List<ExerciseVsBodyPart> ExerciseVsBodyParts { get; set; } = new List<ExerciseVsBodyPart>();
         public List<ExerciseVsPhysio>? ExercisesVsPhysio { get; set; } = new List<ExerciseVsPhysio>();
     }
